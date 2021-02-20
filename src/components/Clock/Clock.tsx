f250bb765ee7a7bd1251f11c6181bafeb5de2d8d
Mark Kadiner <dasistmail765@gmail.com>
2021-02-20 21:56:10 +0300

    added clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+
+type PropsType = {
+
+}
+
+const get2digitString = (num: number) => num < 10 ? '0' + num : num;
+
+export const Clock: React.FC<PropsType> = (props) => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+
+        const intervalID = setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
+
+    return <div>
+        <span>{get2digitString(date.getHours())}</span>
+        <span>:</span>
+        <span>{get2digitString(date.getMinutes())}</span>
+        <span>:</span>
+        <span>{get2digitString(date.getSeconds())}</span>
+    </div>
+}
